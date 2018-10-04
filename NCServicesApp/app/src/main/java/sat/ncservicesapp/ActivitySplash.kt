8978009114fa4017e046b37ee6ec0c2c97c17062
satishjampalwar <Lg-optimus1>
2018-10-04 16:50:22 +0530

    First Commit

---
+++ NCServicesApp/app/src/main/java/sat/ncservicesapp/ActivitySplash.kt
@@ -0,0 +1,22 @@
+package sat.ncservicesapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.view.Window
+import android.view.WindowManager
+
+class ActivitySplash : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window.requestFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        setContentView(R.layout.activity_splash)
+
+       /* val handler = Handler()
+        handler.postDelayed(Runnable {
+
+        },3000)*/
+    }
+}
